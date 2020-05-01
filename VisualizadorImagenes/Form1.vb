ee02baa3a26ceea5e6716f7d361a41504fa0c781
Manuel <manueldocanto777@hotmail.com>
2020-05-01 14:17:27 -0300

    Subida de cambios y proyecto completado

---
+++ VisualizadorImagenes/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    '***************** Manuel Do Canto ********************
+    Private Sub showButton_Click(sender As Object, e As EventArgs) Handles showButton.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            PictureBox1.Load(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        PictureBox1.Image = Nothing
+    End Sub
+
+    Private Sub backroundButton_Click(sender As Object, e As EventArgs) Handles backroundButton.Click
+        If ColorDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.BackColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub closeButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
+        Close()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        Else
+            PictureBox1.SizeMode = PictureBoxSizeMode.Normal
+        End If
+    End Sub
+End Class
